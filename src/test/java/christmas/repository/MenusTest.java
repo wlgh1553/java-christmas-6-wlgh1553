2b373ee69619a705eb30faa47123559ac365c213
wlgh1553 <112055561+wlgh1553@users.noreply.github.com>
2023-11-11 18:15:57 +0900

    test(MenusTest): 없는 메뉴가 들어오는 경우 예외 처리 테스트 추가

--- src/test/java/christmas/repository/MenusTest.java
+++ src/test/java/christmas/repository/MenusTest.java
@@ -1,6 +1,8 @@
 package christmas.repository;
 
+import static christmas.constant.ErrorMessage.INVALID_ORDER;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,6 +22,17 @@ public class MenusTest {
 
         assertThat(menus.getTotalCost()).isEqualTo(14_500);
     }
+    
+    @Test
+    @DisplayName("없는 메뉴가 들어오는 경우 예외가 발생한다.")
+    void noSuchMenuException() {
+        Map<String, Integer> inputOrders = new HashMap<>() {{
+            put("타파스", 1);
+            put("제로사이다", 3);
+        }};
 
-    //없는 메뉴가 들어오는 경우
+        assertThatThrownBy(() -> new Menus(inputOrders))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(INVALID_ORDER.getMessage());
+    }
 }
