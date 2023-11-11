afa69e6887fea962d2a58beec91e52a4e0c339a7
wlgh1553 <112055561+wlgh1553@users.noreply.github.com>
2023-11-11 16:34:33 +0900

    refactor(Day): 날짜에 따른 요일 변경 기능을 private으로 변경

--- src/main/java/christmas/domain/Day.java
+++ src/main/java/christmas/domain/Day.java
@@ -3,23 +3,22 @@ package christmas.domain;
 import static christmas.constant.ErrorMessage.INVALID_DATE;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public enum Day {
-    MON(4),
-    TUE(5),
-    WED(6),
-    THU(0),
-    FRI(1),
-    SAT(2),
-    SUN(3);
+    MON(4), TUE(5), WED(6), THU(0),
+    FRI(1), SAT(2), SUN(3);
 
     private Integer dateMod;
+    private static Set<Integer> specialDays =
+            new HashSet<>(Arrays.asList(3, 10, 17, 24, 25, 31));
 
     Day(Integer dateMod) {
         this.dateMod = dateMod;
     }
 
-    public static Day getDay(int date) {
+    private static Day getDay(int date) {
         checkDateRange(date);
         return Arrays.stream(Day.values())
                 .filter(day -> (date % 7) == day.dateMod)
@@ -33,19 +32,22 @@ public enum Day {
         }
     }
 
-    public boolean isWeekend() {
-        return this == FRI || this == SAT;
+    public static boolean isWeekend(int date) {
+        Day day = getDay(date);
+        return day == FRI || day == SAT;
     }
 
-    public boolean isWeekday() {
-        return !isWeekend();
+    public static boolean isWeekday(int date) {
+        return !isWeekend(date);
     }
 
-    public static boolean isSpecialDay(int date) {
-        return getDay(date) == SUN || date == 25;
+    public static Boolean isSpecialDay(int date) {
+        checkDateRange(date);
+        return specialDays.contains(date);
     }
 
-    public static boolean isDdayPeriod(int date) {
+    public static Boolean isDdayPeriod(int date) {
+        checkDateRange(date);
         return date >= 1 && date <= 25;
     }
 }
--- src/test/java/christmas/domain/DayTest.java
+++ src/test/java/christmas/domain/DayTest.java
@@ -1,8 +1,10 @@
 package christmas.domain;
 
 import static christmas.constant.ErrorMessage.INVALID_DATE;
-import static christmas.domain.Day.MON;
-import static christmas.domain.Day.getDay;
+import static christmas.domain.Day.isDdayPeriod;
+import static christmas.domain.Day.isSpecialDay;
+import static christmas.domain.Day.isWeekday;
+import static christmas.domain.Day.isWeekend;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -11,18 +13,36 @@ import org.junit.jupiter.api.Test;
 
 public class DayTest {
     @Test
-    @DisplayName("날짜가 1이상 31이하가 아닌경우 예외가 발생한다.")
+    @DisplayName("날짜가 1이상 31이하가 아닌데 주말인지 물어보면 예외가 발생한다.")
     void dateExceptionTest() {
         int date = 0;
-        assertThatThrownBy(() -> getDay(date))
+        assertThatThrownBy(() -> isWeekend(date))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(INVALID_DATE.getMessage());
     }
 
     @Test
-    @DisplayName("날짜를 넣으면 요일은 반환한다.")
-    void dateToDayTest() {
+    @DisplayName("날짜를 넣으면 주말인지 주중인지 알려준다.")
+    void weekdayTest() {
         int date = 25;
-        assertThat(getDay(date)).isEqualTo(MON);
+        assertThat(isWeekend(date)).isFalse();
+        assertThat(isWeekday(date)).isTrue();
+    }
+
+    @Test
+    @DisplayName("날짜를 넣으면 별표시된 날짜인지 알려준다.")
+    void specialDayTest() {
+        int specialDate = 3, noSpecialDate = 4;
+        assertThat(isSpecialDay(specialDate)).isTrue();
+        assertThat(isSpecialDay(noSpecialDate)).isFalse();
+    }
+
+    @Test
+    @DisplayName("날짜를 넣으면 디데이 할인 기간인지 알려준다.")
+    void dDayPeriodTest() {
+        int preChristmas = 10;
+        int posChristmas = 30;
+        assertThat(isDdayPeriod(preChristmas)).isTrue();
+        assertThat(isDdayPeriod(posChristmas)).isFalse();
     }
 }
