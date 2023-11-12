c29db13bf69beb79f60f37ad19ef9c49c21d0e14
wlgh1553 <112055561+wlgh1553@users.noreply.github.com>
2023-11-12 19:43:29 +0900

    feat(EventPlanner): 유효한 주문을 입력할 때까지 다시 입력받는 기능

--- src/main/java/christmas/controller/EventPlanner.java
+++ src/main/java/christmas/controller/EventPlanner.java
@@ -3,12 +3,16 @@ package christmas.controller;
 import static christmas.constant.ErrorMessage.INVALID_DATE;
 import static christmas.domain.Day.checkDateRange;
 import static christmas.view.Inputs.getDate;
+import static christmas.view.Inputs.getMenuOrder;
 import static christmas.view.Outputs.showWelcomeMessage;
+
+import christmas.service.MenuService;
 
 public class EventPlanner {
     public EventPlanner() {
         showWelcomeMessage();
         int date = getValidDate();
+        MenuService menuService = getValidMenuService();
     }
 
     private int getValidDate() {
@@ -21,7 +25,16 @@ public class EventPlanner {
         }
     }
 
-
+    private MenuService getValidMenuService() {
+        while (true) {
+            try {
+                return new MenuService(getMenuOrder());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    
     private Integer toNumber(String inputDay) {
         try {
             return Integer.valueOf(inputDay);
